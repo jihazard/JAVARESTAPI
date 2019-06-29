586bd8d493ff783b1838327d9bb0b7eefda56652
jihazard <yoonjh238@gmail.com>
2019-06-29 21:01:59 +0900

    java Rest api init

---
+++ src/main/java/com/jihazardrestapi/demorestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.jihazardrestapi.demorestapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED,BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/com/jihazardrestapi/demorestapi/events/EventTest.java
@@ -0,0 +1,29 @@
+package com.jihazardrestapi.demorestapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+    @Test
+    public void builder() {
+        Event event = Event.builder().build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        Event event = new Event();
+        String name = "EVENT";
+        String description = "SPRING";
+
+        //WHEN
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        assertThat(event.getName()).isEqualTo("EVENT");
+        assertThat(event.getDescription()).isEqualTo("SPRING");
+
+    }
+}
