2da095c2bbce893eb64de7c259d24348dcbae7d8
jihazard <yoonjh238@gmail.com>
2019-07-06 23:37:20 +0900

    1. postgresql 설치 및 연동 2. 운영db(postgresql)과 테스트db(h2) 분리하기

--- src/test/java/com/jihazardrestapi/demorestapi/events/EventTestController.java
+++ src/test/java/com/jihazardrestapi/demorestapi/events/EventTestController.java
@@ -17,6 +17,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.headers.HeaderDocumentation;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocConfiguration.class)
+@ActiveProfiles("test")
 public class EventTestController {
     @Autowired
     MockMvc mockMvc;
