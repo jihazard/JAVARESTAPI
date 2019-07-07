7f6845218eea63e8e5b096821d06baabd16c1399
jihazard <yoonjh238@gmail.com>
2019-07-07 18:22:44 +0900

    리스트 api 등록하기

--- src/main/java/com/jihazardrestapi/demorestapi/events/EventController.java
+++ src/main/java/com/jihazardrestapi/demorestapi/events/EventController.java
@@ -1,19 +1,26 @@
 package com.jihazardrestapi.demorestapi.events;
 
 import com.jihazardrestapi.demorestapi.common.ErrorResource;
+import lombok.var;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -49,9 +56,18 @@ public class EventController {
         Event newEvent = this.eventRepository.save(event);
         ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
         URI createUri = selfLinkBuilder.toUri();
-        System.out.println(event.toString());
         EventResource eventResource = new EventResource(event);
 
         return ResponseEntity.created(createUri).body(eventResource);
+    }
+
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler){
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        var pageResource = assembler.toResource(page , e -> new EventResource(e));
+        pageResource.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok(pageResource);
+
     }
 }
--- src/test/java/com/jihazardrestapi/demorestapi/events/EventTestController.java
+++ src/test/java/com/jihazardrestapi/demorestapi/events/EventTestController.java
@@ -22,6 +22,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -29,6 +30,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.responseH
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -46,6 +48,9 @@ public class EventTestController {
 
     @Autowired
     ObjectMapper objectMapper; //자동으로 빈으로 등록함
+
+    @Autowired
+    EventRepository eventRepository;
 
 //    @MockBean
 //    EventRepository eventRepository;
@@ -76,63 +81,63 @@ public class EventTestController {
                 .andExpect(status().isCreated())//Expect Result : 201
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT))) // return value id가 존재하는지
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-        .andExpect(jsonPath("free").value(false)) // return value id가 존재하는지
-        .andExpect(jsonPath("offline").value(true)) // return value id가 존재하는지
-        .andExpect(jsonPath("_links.self").exists())
-        .andExpect(jsonPath("_links.query-events").exists())
-        .andExpect(jsonPath("_links.update-event").exists())
-        .andDo(document("create-event", links(
-                linkWithRel("self").description("link to self"),
-                linkWithRel("query-events").description("link to query-events"),
-                linkWithRel("update-event").description("link to update-event"),
-                linkWithRel("profile").description("link to profile")
+                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT))) // return value id가 존재하는지
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("free").value(false)) // return value id가 존재하는지
+                .andExpect(jsonPath("offline").value(true)) // return value id가 존재하는지
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-event").exists())
+                .andDo(document("create-event", links(
+                        linkWithRel("self").description("link to self"),
+                        linkWithRel("query-events").description("link to query-events"),
+                        linkWithRel("update-event").description("link to update-event"),
+                        linkWithRel("profile").description("link to profile")
 
 
-                ),
-                requestHeaders(
-                        headerWithName(HttpHeaders.ACCEPT).description("accept header"),
-                        headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
-                ),
-                requestFields(
-                        fieldWithPath("name").description("Name of new event"),
-                        fieldWithPath("description").description("Name of new description"),
-                        fieldWithPath("beginEnrollmentDateTime").description("Name of new beginEnrollmentDateTime"),
-                        fieldWithPath("closeEnrollmentDateTime").description("Name of new closeEnrollmentDateTime"),
-                        fieldWithPath("beginEventDateTime").description("Name of new beginEventDateTime"),
-                        fieldWithPath("endEventDateTime").description("Name of new endEventDateTime"),
-                        fieldWithPath("location").description("Name of new location"),
-                        fieldWithPath("basePrice").description("Name of new basePrice"),
-                        fieldWithPath("maxPrice").description("Name of new maxPrice"),
-                        fieldWithPath("limitOfEnrollment").description("Name of new limitOfEnrollment")
-                )
-                ,responseHeaders(
-                        headerWithName(HttpHeaders.LOCATION).description("location header"),
-                        headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
-                )
-                ,responseFields(
-                        fieldWithPath("id").description("Name of new id"),
-                        fieldWithPath("name").description("Name of new event"),
-                        fieldWithPath("description").description("Name of new description"),
-                        fieldWithPath("beginEnrollmentDateTime").description("Name of new beginEnrollmentDateTime"),
-                        fieldWithPath("closeEnrollmentDateTime").description("Name of new closeEnrollmentDateTime"),
-                        fieldWithPath("beginEventDateTime").description("Name of new beginEventDateTime"),
-                        fieldWithPath("endEventDateTime").description("Name of new endEventDateTime"),
-                        fieldWithPath("location").description("Name of new location"),
-                        fieldWithPath("basePrice").description("Name of new basePrice"),
-                        fieldWithPath("maxPrice").description("Name of new maxPrice"),
-                        fieldWithPath("limitOfEnrollment").description("Name of new limitOfEnrollment"),
-                        fieldWithPath("free").description("Name of new free"),
-                        fieldWithPath("offline").description("Name of new offline"),
-                        fieldWithPath("eventStatus").description("Name of new eventStatus"),
-                        fieldWithPath("_links.self.href").description("link to self"),
-                        fieldWithPath("_links.query-events.href").description("link to query-events"),
-                        fieldWithPath("_links.update-event.href").description("link to update-event"),
-                        fieldWithPath("_links.profile.href").description("link to profile")
-                )
+                        ),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
+                        ),
+                        requestFields(
+                                fieldWithPath("name").description("Name of new event"),
+                                fieldWithPath("description").description("Name of new description"),
+                                fieldWithPath("beginEnrollmentDateTime").description("Name of new beginEnrollmentDateTime"),
+                                fieldWithPath("closeEnrollmentDateTime").description("Name of new closeEnrollmentDateTime"),
+                                fieldWithPath("beginEventDateTime").description("Name of new beginEventDateTime"),
+                                fieldWithPath("endEventDateTime").description("Name of new endEventDateTime"),
+                                fieldWithPath("location").description("Name of new location"),
+                                fieldWithPath("basePrice").description("Name of new basePrice"),
+                                fieldWithPath("maxPrice").description("Name of new maxPrice"),
+                                fieldWithPath("limitOfEnrollment").description("Name of new limitOfEnrollment")
+                        )
+                        , responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("location header"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        )
+                        , responseFields(
+                                fieldWithPath("id").description("Name of new id"),
+                                fieldWithPath("name").description("Name of new event"),
+                                fieldWithPath("description").description("Name of new description"),
+                                fieldWithPath("beginEnrollmentDateTime").description("Name of new beginEnrollmentDateTime"),
+                                fieldWithPath("closeEnrollmentDateTime").description("Name of new closeEnrollmentDateTime"),
+                                fieldWithPath("beginEventDateTime").description("Name of new beginEventDateTime"),
+                                fieldWithPath("endEventDateTime").description("Name of new endEventDateTime"),
+                                fieldWithPath("location").description("Name of new location"),
+                                fieldWithPath("basePrice").description("Name of new basePrice"),
+                                fieldWithPath("maxPrice").description("Name of new maxPrice"),
+                                fieldWithPath("limitOfEnrollment").description("Name of new limitOfEnrollment"),
+                                fieldWithPath("free").description("Name of new free"),
+                                fieldWithPath("offline").description("Name of new offline"),
+                                fieldWithPath("eventStatus").description("Name of new eventStatus"),
+                                fieldWithPath("_links.self.href").description("link to self"),
+                                fieldWithPath("_links.query-events.href").description("link to query-events"),
+                                fieldWithPath("_links.update-event.href").description("link to update-event"),
+                                fieldWithPath("_links.profile.href").description("link to profile")
+                        )
 
-        ))
+                ))
         ;
 
     }
@@ -169,7 +174,7 @@ public class EventTestController {
 
     @Test
     @TestDescription("배드 입력값이 비어있을 경우")
-    public void createEvent_Bad_Request_Empty_Input()throws Exception{
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
         this.mockMvc.perform(post("/api/events")
@@ -181,7 +186,7 @@ public class EventTestController {
 
     @Test
     @TestDescription("배드 리퀘스트 요청이 잘못되었을 경우")
-    public void createEvent_Bad_Request_Wrong_Input()throws Exception{
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder().name("Spring")
                 .description("REST API Development")
                 .beginEventDateTime(LocalDateTime.of(2019, 07, 02, 11, 14))
@@ -201,7 +206,32 @@ public class EventTestController {
                 .andExpect(jsonPath("$[0].objectName").exists())
                 .andExpect(jsonPath("$[0].defaultMessage").exists())
                 .andExpect(jsonPath("$[0].code").exists())
-               ;
+        ;
     }
 
+
+    @Test
+    @TestDescription("30개의 이벤트를 10개씩 두번째 페이지 조회")
+    public void getQueryEvents() throws Exception {
+        //Given
+        IntStream.range(0, 30).forEach(this::generateEvent);
+        //when
+        this.mockMvc.perform(get("/api/events")
+                .param("page", "1")
+                .param("size", "10")
+                .param("sort", "name,DESC")
+        ).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("page").exists())
+                .andExpect(jsonPath("_embedded.eventList[1]._links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andDo(document("query-events"))
+        ;
+        //then
+    }
+
+    private void generateEvent(int i) {
+        Event event = Event.builder().name("event " + i).description("event test").build();
+        this.eventRepository.save(event);
+    }
 }
+
